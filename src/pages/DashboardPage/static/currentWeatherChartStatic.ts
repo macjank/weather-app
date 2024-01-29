9ddcf61a6137cff56f98b6edbfc94bcfc6c0fedd
Maciek <maciek.duzy.11@gmail.com>
2024-01-29 22:21:58 +0100

    feat: update max wind speed range

--- src/pages/DashboardPage/static/currentWeatherChartStatic.ts
+++ src/pages/DashboardPage/static/currentWeatherChartStatic.ts
@@ -4,7 +4,7 @@ export const CHART_MAX_SCALE = 10;
 export const currentWeatherRange = {
   temperatureRange: { min: -10, max: 40 },
   humidityRange: { min: 0, max: 100 },
-  windSpeedRange: { min: 0, max: 50 },
+  windSpeedRange: { min: 0, max: 80 },
 };
 
 export const chartBackgroundColors = [
