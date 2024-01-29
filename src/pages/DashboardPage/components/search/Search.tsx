4f8471a990b169ae2ab89aa0600a5ab591851a60
Maciek <maciek.duzy.11@gmail.com>
2024-01-29 20:39:56 +0100

    refactor: results list toggle

--- src/pages/DashboardPage/components/search/Search.tsx
+++ src/pages/DashboardPage/components/search/Search.tsx
@@ -3,11 +3,8 @@ import { useTranslation } from 'react-i18next';
 import { Input } from '../../../../components/inputs/Input';
 import { useWeatherSearch } from '../../../../hooks/api/useWeatherSearch';
 import { useDebounce } from '../../../../hooks/useDebounce';
-import { useOnClickOutside } from '../../../../hooks/useOnClickOutside';
 import { ISearchResult } from '../../../../types/WeatherTypes/WeatherTypes';
 import SearchResultsList from './SearchResultsList';
-
-const ID = 'search-results';
 
 interface SearchProps {
   onSelectPlace: (place: ISearchResult) => void;
@@ -29,18 +26,16 @@ const Search = ({ onSelectPlace }: SearchProps) => {
 
   const handleSelectResult = (result: ISearchResult) => {
     onSelectPlace(result);
-    setIsResultListOpen(false);
   };
 
-  useOnClickOutside({ targetId: ID, onClick: () => setIsResultListOpen(false) });
-
   return (
-    <div id={ID} className="relative">
+    <div className="relative">
       <Input
         placeholder={t('dashboard.search.placeholder')}
         value={searchValue}
         onChange={handleChangeSearchValue}
-        onClick={() => setIsResultListOpen(true)}
+        onFocus={() => setIsResultListOpen(true)}
+        onBlur={() => setIsResultListOpen(false)}
       />
       {isResultListOpen && (
         <SearchResultsList results={searchResults} onSelectResult={handleSelectResult} isLoading={isLoading} />
